b1b19669410f0379e8d88a8bc320c6ba1c9451f7
Mark LaDoux <mark.ladoux@gmail.com>
2018-02-04 21:46:05 -0600

    messing with containers

--- config/containers.php
+++ config/containers.php
@@ -1,1 +1,12 @@
 <?php
+$container = $app->getContainer();
+
+// Custom Errors
+$container['notFoundHandler'] = function($container) {
+	return function ($request, $response) use ($container) {
+		return $container['response']
+			->withStatus(404)
+			->withHeader('Content-Type', 'text/html')
+			->write('Page not found');
+	};
+};
